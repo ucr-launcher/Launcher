30a53f30a216a99a071e148b62013324742a9173
AaronSigal <asigal55@gmail.com>
2019-04-30 12:09:51 -0700

    Added another tuning parameter for a flat adjustment

--- trajectory.cpp
+++ trajectory.cpp
@@ -15,17 +15,23 @@ const float x_scalar = 1.0; // platform rotation
 const float y_scalar = 1.0; // platform vertical orientation
 const float z_scalar = 1.0; // PWM speed
 
-//@Params: 1 = the location of the target on the x-axis (in pixels) | 2 = the location of the target on the y-axis (in pixels)|  3 = the location of the target on the z-axis (in mm)
-void find_trajectory(vector<int> location_data) {  
+//Flat values which can be appended to the values to finely-tune the trajectories.
+const float x_const = 0.0;
+const float y_const = 0.0;
+const float z_const = 0.0;
+
+//@Params: a vector who's 0th index is the x location data, 1st index is the y location data, and the 2nd index is the z location data.
+void find_trajectory(vector<int> &location_data) {  
 
 	if (location_data.size() < 3) { //check if the input vector has the correct size
 		cout << "Error: Location_data vector has wrong size! Expected: 3. Got: " << location_data.size() << endl;
 		return;
 	}
 	
-	location_data.at(0) = location_data.at(0) * x_scalar;
-	location_data.at(1) = location_data.at(1) * y_scalar;
-	location_data.at(2) = location_data.at(2) * z_scalar;
+	//Transform the vector's contents using the scalars
+	location_data.at(0) = location_data.at(0) * x_scalar + x_const;
+	location_data.at(1) = location_data.at(1) * y_scalar + y_const;
+	location_data.at(2) = location_data.at(2) * z_scalar + z_const;
 	
 }
 
@@ -49,8 +55,6 @@ int main() {
 	
 	find_trajectory(location_data);
 	
-	cout << "Printing data.." << endl;
-	cout << "launch_data length: " + location_data.size() << endl;
 	for (int i : location_data) {
 		cout << i << endl;
 	}
