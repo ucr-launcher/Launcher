b1be1f3bbcaf0916779dfc6ae9301f9d380c656b
AaronSigal <asigal55@gmail.com>
2019-05-02 11:37:07 -0700

    Renamed variables to add clarity, changed vectors to float type

--- trajectory.cpp
+++ trajectory.cpp
@@ -11,9 +11,9 @@ const int image_frame_height = 400; //height of the image from which the traject
 const int maximum_depth = 5791;     //maximum depth that the sensor could read (in mm)
 
 //Scalars for tuning the launching algorithm.
-const float x_scalar = 1.0; // platform rotation
-const float y_scalar = 1.0; // platform vertical orientation
-const float z_scalar = 1.0; // PWM speed
+const float x_scalar = 1.3; // platform rotation
+const float y_scalar = 1.2; // platform vertical orientation
+const float z_scalar = 0.5; // PWM speed
 
 //Flat values which can be appended to the values to finely-tune the trajectories.
 const float x_const = 0.0;
@@ -21,17 +21,17 @@ const float y_const = 0.0;
 const float z_const = 0.0;
 
 //@Params: a vector who's 0th index is the x location data, 1st index is the y location data, and the 2nd index is the z location data.
-void find_trajectory(vector<int> &location_data) {  
+void find_trajectory(vector<float> &data) {  
 
-	if (location_data.size() < 3) { //check if the input vector has the correct size
-		cout << "Error: Location_data vector has wrong size! Expected: 3. Got: " << location_data.size() << endl;
+	if (data.size() < 3) { //check if the input vector has the correct size
+		cout << "Error: Location_data vector has wrong size! Expected: 3. Got: " << data.size() << endl;
 		return;
 	}
 	
 	//Transform the vector's contents using the scalars
-	location_data.at(0) = location_data.at(0) * x_scalar + x_const;
-	location_data.at(1) = location_data.at(1) * y_scalar + y_const;
-	location_data.at(2) = location_data.at(2) * z_scalar + z_const;
+	data.at(0) = data.at(0) * x_scalar + x_const;
+	data.at(1) = data.at(1) * y_scalar + y_const;
+	data.at(2) = data.at(2) * z_scalar + z_const;
 	
 }
 
@@ -47,7 +47,7 @@ int main() {
 	//*** End of setup ***//
 	
 	cout << "Running algorithm" << endl;
-	vector<int> location_data;
+	vector<float> location_data;
 	location_data.push_back(x_loc);
 	location_data.push_back(y_loc);
 	location_data.push_back(z_loc);
