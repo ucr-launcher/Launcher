2bdc9f17f0a0be36af78bb1ae7740a20ac3b7bd2
AaronSigal <asigal55@gmail.com>
2019-05-09 10:15:57 -0700

    Renamed variables and adjusted constants to be more consistent

--- trajectory.h
+++ trajectory.h
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-const int image_frame_width  = 600; //width of the image from which the trajectory was pulled (in pixels)
-const int image_frame_height = 400; //height of the image from which the trajectory data was pulled (in pixels)
-const int maximum_depth = 5791;     //maximum depth that the sensor could read (in mm)
+const int x_max  = 640; //width of the image from which the trajectory was pulled (in pixels)
+const int y_max  = 420; //height of the image from which the trajectory data was pulled (in pixels)
+const int z_max  = 100; //Maximum PWM speed that the DC motors can spin at. Since the period is unknown at this time, the maximum values are also unknown.
 
 //Scalars for tuning the launching algorithm.
-const float x_scalar = 1.3; // platform rotation
-const float y_scalar = 1.2; // platform vertical orientation
-const float z_scalar = 0.5; // PWM speed
+const float x_scalar = 0.1; // platform rotation
+const float y_scalar = 0.1; // platform vertical orientation
+const float z_scalar = 0.1; // PWM speed
 
 //Flat values which can be appended to the values to finely-tune the trajectories.
 const float x_const = 0.0;
