c32c4908e07771bb90827957b5b63a2a04c56e1b
rvazq002 <rvazq002@users.noreply.github.com>
2019-05-13 18:35:28 -0700

    Updated roi_extractor.py v2.0
    roi_extractor now uses pretrained model for object detection
    
    Dependency: Tensorflow Object Detection API installed

--- roi_extractor.py
+++ roi_extractor.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python
-import rospy
+# coding: utf-8
 import numpy as np
+import os
+import sys
+import six.moves.urllib as urllib
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+import tarfile
+import tensorflow as tf
+import time
+import copy
+
+from tensorflow.core.framework import graph_pb2
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from object_detection.utils import ops as utils_ops
+#from matplotlib import pyplot as plt
+from PIL import Image
+from io import StringIO
+from collections import defaultdict
 import cv2
-from std_msgs.msg import UInt16
+
+import rospy
+from std_msgs.msg import UInt16, Int16
 from sensor_msgs.msg import Image
-from senior_design.msg import roi_image
 from cv_bridge import CvBridge, CvBridgeError
+
+print "completed imports"
+
+#cap = cv2.VideoCapture(0)
 
 # Ros Image data converter
 bridge = CvBridge()
@@ -13,15 +36,116 @@ bridge = CvBridge()
 global rgbImg
 rgbImg = np.zeros(shape=(480,640,3))
 
+##-------------------------------------------------------------------------------------------------
+##-------------------------------------------------------------------------------------------------
+
+def _node_name(n):
+  if n.startswith("^"):
+    return n[1:]
+  else:
+    return n.split(":")[0]
+
+
+input_graph = tf.Graph()
+with tf.Session(graph=input_graph):
+    score = tf.placeholder(tf.float32, shape=(None, 1917, 90), name="Postprocessor/convert_scores")
+    expand = tf.placeholder(tf.float32, shape=(None, 1917, 1, 4), name="Postprocessor/ExpandDims_1")
+    for node in input_graph.as_graph_def().node:
+        if node.name == "Postprocessor/convert_scores":
+            score_def = node
+        if node.name == "Postprocessor/ExpandDims_1":
+            expand_def = node
+
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile('./ssd_mobilenet_v1_coco_2017_11_17/frozen_inference_graph.pb', 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    dest_nodes = ['Postprocessor/convert_scores','Postprocessor/ExpandDims_1']
+
+    edges = {}
+    name_to_node_map = {}
+    node_seq = {}
+    seq = 0
+    for node in od_graph_def.node:
+      n = _node_name(node.name)
+      name_to_node_map[n] = node
+      edges[n] = [_node_name(x) for x in node.input]
+      node_seq[n] = seq
+      seq += 1
+
+    for d in dest_nodes:
+      assert d in name_to_node_map, "%s is not in graph" % d
+
+    nodes_to_keep = set()
+    next_to_visit = dest_nodes[:]
+    while next_to_visit:
+      n = next_to_visit[0]
+      del next_to_visit[0]
+      if n in nodes_to_keep:
+        continue
+      nodes_to_keep.add(n)
+      next_to_visit += edges[n]
+
+    nodes_to_keep_list = sorted(list(nodes_to_keep), key=lambda n: node_seq[n])
+
+    nodes_to_remove = set()
+    for n in node_seq:
+      if n in nodes_to_keep_list: continue
+      nodes_to_remove.add(n)
+    nodes_to_remove_list = sorted(list(nodes_to_remove), key=lambda n: node_seq[n])
+
+    keep = graph_pb2.GraphDef()
+    for n in nodes_to_keep_list:
+      keep.node.extend([copy.deepcopy(name_to_node_map[n])])
+
+    remove = graph_pb2.GraphDef()
+    remove.node.extend([score_def])
+    remove.node.extend([expand_def])
+    for n in nodes_to_remove_list:
+      remove.node.extend([copy.deepcopy(name_to_node_map[n])])
+
+    with tf.device('/gpu:0'):
+      tf.import_graph_def(keep, name='')
+    with tf.device('/cpu:0'):
+      tf.import_graph_def(remove, name='')
+
+
+NUM_CLASSES = 90
+label_map = label_map_util.load_labelmap('data/mscoco_label_map.pbtxt')
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+##-----------------------------------------------------------------------------------------------##
+##-----------------------------------------------------------------------------------------------##
+
 #--------------------- ROS NODE CONFIGURATION -------------------------------
 rospy.init_node('object_detector', anonymous=False)
 
 # Set up Publishers
-roi = roi_image()
-roi.x = 0
-roi.y = 0
+x_loc = Int16()
+x_loc.data = 0
 
-roi_publisher = rospy.Publisher('/roi_location', roi_image, queue_size=1)
+xloc_publisher = rospy.Publisher('/xaxis_location', Int16, queue_size=1)
+
 
 # Set up Subscribers
 def image_callback(image_msg):
@@ -34,106 +158,60 @@ image_subscriber = rospy.Subscriber('/camera/rgb/image_color', Image, image_call
 
 # Set loop rate in Hz  
 rate = rospy.Rate(20)
+
 #----------------------- END CONFIGURATION ----------------------------------
 
 
-#-------------------- OBJECT DETECTION PIPELINE -----------------------------
-def object_detector():
-
-	def nothing(x):
-		pass
-
-	cv2.namedWindow('result')
-
-	# h  16 s  153 v  142
-	# h1 45 s1 255 v1 255
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph,config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    score_out = detection_graph.get_tensor_by_name('Postprocessor/convert_scores:0')
+    expand_out = detection_graph.get_tensor_by_name('Postprocessor/ExpandDims_1:0')
+    score_in = detection_graph.get_tensor_by_name('Postprocessor/convert_scores_1:0')
+    expand_in = detection_graph.get_tensor_by_name('Postprocessor/ExpandDims_1_1:0')
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    i = 0
+    while not rospy.is_shutdown():
+      #ret, image_np = cap.read()
+      image_np = rgbImg
+      
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+    
+      start_time = time.time()
+      (score, expand) = sess.run([score_out, expand_out], feed_dict={image_tensor: image_np_expanded})
+      (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={score_in:score, expand_in: expand})
+      ##print 'Iteration %d: %.3f sec'%(i, time.time()-start_time)
+      i += 1
+      vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+       np.squeeze(boxes),
+      np.squeeze(classes).astype(np.int32),
+      np.squeeze(scores),
+      category_index,
+      use_normalized_coordinates=True,
+      line_thickness=8)
 	
-	h = 100
-	s = 055
-	v = 106
-
-	h1 = 166
-	s1 = 178
-	v1 = 255
- 
-	# Creating track bar
-	cv2.createTrackbar('h', 'result',0,179, nothing)
-	cv2.createTrackbar('s', 'result',0,255, nothing)
-	cv2.createTrackbar('v', 'result',0,255, nothing)
-	cv2.createTrackbar('h1', 'result',0,179, nothing)
-	cv2.createTrackbar('s1', 'result',35,255, nothing)
-	cv2.createTrackbar('v1', 'result',0,255, nothing)
-
-	cv2.setTrackbarPos('h','result',100)
-	cv2.setTrackbarPos('s','result',055)
-	cv2.setTrackbarPos('v','result',106)
-	cv2.setTrackbarPos('h1','result',166)
-	cv2.setTrackbarPos('s1','result',178)
-	cv2.setTrackbarPos('v1','result',255)
-
-	while not rospy.is_shutdown():
-		try:
-			# Convert to hsv color space
-			hsvImg = cv2.cvtColor(rgbImg, cv2.COLOR_BGR2HSV)
+      # Iterate through boxes and look for sports ball!!!
+      for i,b in enumerate(boxes[0]):
+      	if classes[0][i] == 1 or classes[0][i] == 37:
+	  mid_x = (boxes[0][i][1]+boxes[0][i][3])/2
+          mid_y = (boxes[0][i][0]+boxes[0][i][2])/2
+	  
+	  if scores[0][i] >= 0.5: 
+	    print "Person at: " + str(mid_x*800) + ", " + str(mid_y*600)
 		
-			# get info from track bar and appy to result
-			h = cv2.getTrackbarPos('h','result')
-			s = cv2.getTrackbarPos('s','result')
-			v = cv2.getTrackbarPos('v','result')
+            # Publish x-axis
+	    x_loc.data = mid_x*800
+            xloc_publisher.publish(x_loc) 
 
-			h1 = cv2.getTrackbarPos('h1','result')
-			s1 = cv2.getTrackbarPos('s1','result')
-			v1 = cv2.getTrackbarPos('v1','result')
-
-
-			# Color based Threshold
-			lower_orange = np.array([h,s,v])
-			upper_orange = np.array([h1,s1,v1])
-		
-			thresh_img = cv2.inRange(hsvImg, lower_orange, upper_orange)
-
-			result = cv2.bitwise_and(rgbImg, rgbImg, mask = thresh_img)
-			result = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-			
-			# Circle Detection
-			circles = cv2.HoughCircles(result,cv2.HOUGH_GRADIENT,
-				   1,400,param1=20,param2=50,
-				   minRadius=10 ,maxRadius=300)
-			
-			# Publish ROI (x,y) location 
-			x = 0
-			y = 0
-
-			if circles is not  None:
-				circles = np.uint16(np.around(circles))
-				for i in circles[0,:]:
-					# Draw Outer circle
-					# parameters: (frame, x, y, radius, color, thickness)
-					cv2.circle(rgbImg,(i[0],i[1]),i[2],(0,255,0),2)
-					# Draw Centroid
-					cv2.circle(rgbImg,(i[0],i[1]),2,(0,0,255),3)
-					x = i[0]
-					y = i[1]
-					text = '(' + str(i[0]) + ', ' + str(i[1]) + ')'
-					cv2.putText(rgbImg,text,(i[0],i[1]),cv2.FONT_ITALIC,1, 255)
-	
-			print('Xpos: {}, Ypos: {} '.format(x, y))
-			
-			# Display result	
-			cv2.imshow("object_detected", rgbImg)
-			cv2.imshow("thresh_image", result)
-			cv2.waitKey(3)
-
-		except :
-			pass	
-		
-		rate.sleep()
-#-------------------- END DETECTION PIPELINE -------------------------------
-
-
-#------------------------- MAIN LOOP ----------------------------------------
-if __name__ == "__main__":
-	try:
-		object_detector()
-	except rospy.ROSInterruptException:
-		pass
+      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+	cv2.destroyAllWindows()
+	break
+    
+    
