b0b5335b8e84399446367c99390b8ea371f6b23b
rvazq002 <rvazq002@users.noreply.github.com>
2019-05-27 12:48:47 -0700

    updated orient_x state

--- stm_communication.cpp
+++ stm_communication.cpp
@@ -5,18 +5,24 @@
 //--------- GLOBAL VARIABLES LOCAL TO STM32 MAIN -----------
 DigitalOut my_led(LED1);
 
-int xaxis_location       = 0;
-int num_steps            = 0;
-double kinect_fov_middle = 28.5;
-double degrees_per_step  = 1.8;
+int xaxis_location            = 0;
+int num_steps                 = 0;
+int prev_steps                = 0;
+double kinect_fov_middle      = 28.5;
+double degrees_per_step       = 1;
+int direction_flag            = 0;
 
-int direction            = 0;
 
-
+/*
 DigitalOut D_8(D8);
 DigitalOut D_9(D9);
 DigitalOut D_10(D12);
 DigitalOut D_11(D11);
+*/
+
+DigitalOut motor(D8);
+DigitalOut direction(D9);
+
 
 //--------  END GLOBAL VARIABLES DECLARATION ----------------
 
@@ -28,19 +34,19 @@ float map(float x, float in_min, float in_max, float out_min, float out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-// Maps pixels to number of steps needed to turn n degrees.
+// Maps pixels to number of steps needed to turn n degrees. 
 // Returns a 1 or 0 to determine whether to make left or right turns.
 int map_orientation(int x){
     // Pixels to degrees
     double degrees = 0;
     degrees = map(x, 0, 800, 0,57);
-
+    
     // degrees to steps
     num_steps = (kinect_fov_middle - degrees)/ degrees_per_step;
-
+    
     // Left or right turn
     if (num_steps < 0){
-        num_steps = num_steps * -1;
+        num_steps = num_steps*-1;
         return 1;
     }else{
         return 0;
@@ -51,127 +57,133 @@ int map_orientation(int x){
 
 
 //--------  STEPPER MOTOR HELPER FUNCTIONS   ----------------
-void turn_left_n_steps(int steps){
-    for (int i = 0; i < steps; i++) {
 
-        D_8.write(1);
-        D_9.write(1);
-        D_10.write(0);
-        D_11.write(0);
-
-        wait_ms(10);
-
-        D_8.write(0);
-        D_9.write(1);
-        D_10.write(1);
-        D_11.write(0);
-
-        wait_ms(10);
-
-        D_8.write(0);
-        D_9.write(0);
-        D_10.write(1);
-        D_11.write(1);
-
-        wait_ms(10);
-
-        D_8.write(1);
-        D_9.write(0);
-        D_10.write(0);
-        D_11.write(1);
-
-        wait_ms(10);
-    }
-
-}
-
-void turn_right_n_steps(int steps) {
-       for (int i = 0; i < steps; i++) {
-
-        D_8.write(1);
-        D_9.write(0);
-        D_10.write(0);
-        D_11.write(1);
-
-        wait_ms(10);
-
-        D_8.write(0);
-        D_9.write(0);
-        D_10.write(1);
-        D_11.write(1);
-
-        wait_ms(10);
-
-        D_8.write(0);
-        D_9.write(1);
-        D_10.write(1);
-        D_11.write(0);
-
-        wait_ms(10);
-
-        D_8.write(1);
-        D_9.write(1);
-        D_10.write(0);
-        D_11.write(0);
-
-        wait_ms(10);
+void turn_stepper_left(int steps){
+    direction.write(0);
+    for(int i = 0; i < steps; i++){
+        motor.write(0);
+        wait_ms(1);
+        motor.write(1);
     }
 }
 
+void turn_stepper_right(int steps){
+    direction.write(1);
+    for(int i = 0; i < steps; i++){
+        motor.write(0);
+        wait_ms(1);
+        motor.write(1);
+    }
+}
 
-//--------------  END HELPER FUNCTION -----------------------
-enum States { Detect, Orient_x, Orient_z, Orient_y, Launch, Zero } state;
+//--------------  END HELPER FUNCTION ----------------------- 
+
+//--------------  LAUNCHING MECHANISM SM --------------------
+
+enum States { detect, orient_x, orient_z, orient_y, launch, zero } state;
 
 
 void launcher_sm(){
-    int x = 0;
+
+    
+    Timer t;
+    
+    // Transitions
     switch(state) {
-        case Detect:
-            x = 1;
+        case detect:
+            // Keep searching until object is detected
+            if (xaxis_location != 0)
+            {
+                state = orient_x;
+            }else{
+                state = detect;   
+            }
             break;
-        case Orient_x:
-            x = 2;
+            
+        case orient_x:
+            // Proceed to orient x-axis
+            state = orient_y;
+            break;   
+             
+        case orient_y:
+            // Proceed to orient z-axis
+            state = orient_z;
             break;
-        case Orient_z:
-            x = 3;
+            
+        case orient_z:
+            // Proceed to launch projectile
+            state = launch;
+            break;           
+            
+        case launch:
+            state = zero; 
+            
             break;
-        case Orient_y:
-            x = 4;
+        case zero:
+            x = 6;
             break;
     }
-
+    // Actions
     switch(state){
-        case Detect:
+        case detect:
             x = 1;
             break;
-        case Orient_x:
-            x = 2;
+            
+        case orient_x:
+        
+            // Value returned is a 1 or 0 to determin Left or Right turns-------
+            direction_flag = map_orientation(xaxis_location);
+        
+            // Check if the current steps to take are not the same as the previous
+            // If the prev steps and current steps are the same, the object is at
+            // the same position
+            if ( abs(num_steps - prev_steps) > 5) {
+                if (direction_flag){
+                    turn_stepper_right(num_steps);
+                } else if(!direction_flag){
+                    turn_stepper_left(num_steps);
+                }
+            }
+        
+            // Assign current steps to prev steps
+            prev_steps = num_steps;
+            
             break;
-        case Orient_y:
+            
+        case orient_y:
             x = 3;
             break;
-        case Launch:
-            x = 4;
+            
+        case orient_z:
+            x = 5;
             break;
-        case Zero:
+            
+        case launch:
+            t.start();
+            while(t.read() < 7){
+                // do nothing    
+            }
+            t.stop();
+            t.reset();
+            break;
+            
+        case zero:
             x = 5;
             break;
     }
 }
 
+//-------------- END STATE MACHINE --------------------------
 
-
-
-
-//--------- ROS NODE CONFIGURATION -------------------------
+//---------- ROS NODE CONFIGURATION -------------------------
 ros::NodeHandle nh;
 
 // Ros message data containers
 std_msgs::Int16 transmit_msg;
 
 // Set up Publishers
-//ros::Publisher acknowledge_publisher("acknowledge", &transmit_msg);
-
+ros::Publisher acknowledge_publisher("num_steps", &transmit_msg);
+            
 // Set up Subscribers
 void xaxis_callback( const std_msgs::Int16& msg)
 {
@@ -185,43 +197,29 @@ ros::Subscriber<std_msgs::Int16> xaxis_subscriber("xaxis_location", xaxis_callba
 //---------- END CONFIGURATION  ----------------------------
 
 int main() {
-
+    
     // Initialize Node and publisher/subscriber
     nh.initNode();
-    //nh.advertise(acknowledge_publisher);
+    nh.advertise(acknowledge_publisher);
     nh.subscribe(xaxis_subscriber);
-
+    
+    state = detect;
+    
     while(1) {
-
-        // Use local variables for computations here
-        if (xaxis_location == 0)
-        {
-            // do nothing
-        }
-        else if (xaxis_location > 320)
-        {
-            turn_right_n_steps(5);
-        }
-        else if (xaxis_location < 320)
-        {
-            turn_left_n_steps(5);
-        }
-
-        //---------------------- FIXME -- TEST --------------------------
-        // Value returned is a 1 or 0 to determin Left or Right turns
-        direction = map_orientation(xaxis_location);
-
-        if (direction){
-            turn_right_n_steps(num_steps);
-        }else if(!direction){
-            turn_left_n_steps(num_steps);
-        }
+        
+  
         //----------------------------------------------------------------
-
+        
+        // Call state machine
+        launcher_sm();
+        
+        
         // Publish any data necessary to be fed back to pc
-        //transmit_msg.data = xaxis_location;
-        //acknowledge_publisher.publish(&transmit_msg);
-
+        transmit_msg.data = num_steps;
+        acknowledge_publisher.publish(&transmit_msg);
+        
+        
+        
         // Spin ROS Node -- get data from callbacks
         nh.spinOnce();
         wait_ms(5);
